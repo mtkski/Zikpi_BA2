dfc58d19d3b6c122614272c4fc57aa5507ea76ef
PereBlaise <99497144+PereBlaise@users.noreply.github.com>
2022-03-02 15:44:03 +0100

    Update boucle.py

--- boucle.py
+++ boucle.py
@@ -1,3 +1,32 @@
+#Les imports
+import RPi.GPIO as GPIO
+from CONFIG import Encodeur, Bouton, GEN, SEQ
+
+# Pour déterminer quel système de numérotation des pins on utilise
+GPIO.setmode(GPIO.BCM)
+
+# Crée une liste d'objet button et configure chaque canal comme entrée ou sortie pour les boutons
+button_list = []
+for button_name in Bouton.keys():
+    PIN = Bouton[button_name]
+    GPIO.setup(PIN, GPIO.IN)
+    button = Button(button_name, PIN)
+    button_list.append(button)
+
+# Crée une liste d'objet encoder et configure chaque canal comme entrée ou sortie pour les encodeurs
+encoder_list = []
+for encoder_name in Encodeur.keys():
+    CLK = Encodeur[encoder_name][0]
+    DT = Encodeur[encoder_name][1]
+    SW = Encodeur[encoder_name][2]
+    GPIO.setup(CLK, GPIO.IN)
+    GPIO.setup(DT, GPIO.IN)
+    if SW is not None:
+        GPIO.setup(SW, GPIO.IN)
+    encoder = Encoder(encoder_name, CLK, DT, SW)
+    encoder_list.append(encoder)
+
+
 # Boucle
 while True:
   """
