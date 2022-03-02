59c838608a0961267b074aa9d12a28be6497107d
PereBlaise <99497144+PereBlaise@users.noreply.github.com>
2022-03-02 15:41:00 +0100

    Update boucle.py

--- boucle.py
+++ boucle.py
@@ -1,1 +1,18 @@
+# Boucle
+while True:
+  """
+  Prise de valeur
+  """
+    for encoder in encoder_list:
+        if encoder.motion_sensor() == "button pressed":
+            GEN, SEQ = encoder.dictionary_modification(None, GEN, SEQ)
 
+        elif encoder.motion_sensor() == "rotated clockwise":
+            GEN, SEQ = encoder.dictionary_modification("+", GEN, SEQ)
+
+        elif encoder.motion_sensor() == "rotated counter-clockwise":
+            GEN, SEQ = encoder.dictionary_modification("-", GEN, SEQ)
+
+    for button in button_list:
+        if button.motion_sensor() == "button pressed":
+            GEN = button.dictionary_modification(GEN)
