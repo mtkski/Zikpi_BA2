0e71f3c71fa65bd0fd04c99a68b08465ec065e15
Arkadixit <83871882+Arkadixit@users.noreply.github.com>
2022-03-03 15:44:42 +0100

    Update traduction_note.py

--- traduction_note.py
+++ traduction_note.py
@@ -13,3 +13,54 @@ def Note(num_note):
 
 for i in range(1,61,1) :
     print(Note(i))
+    
+
+class Gamme :
+    
+    def __init__(self, num_gam) :
+        
+        if num_gam == 1 :
+            self.name = "Chromatique"
+        elif num_gam == 2 :
+            self.name = "Majeur"
+        elif num_gam == 3 :
+            self.name = "Mineur"
+            
+    def chiffretonum(SEQ) :  
+        dico_note,num = {}, 1
+    
+        for pas in SEQ.keys() :
+            chiffre_note = SEQ[pas]["note"]
+            num_note = int(chiffre_note*60/4096 + 0.99)
+            dico_note["note{}".format(num)] = num_note
+            num +=1
+        
+        return dico_note
+
+    def transformation_affichage(dico_note) :
+        
+        if self.name == "Chromatique" :
+            for note in dico_note.keys() :
+                Lettre_note = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+                octave = int((dico_note[note]*5)/60 + 0.99)
+                lettre = dico_note[note] % 12
+                dico_note[note] = Lettre_note[lettre - 1] + str(octave)
+                
+        elif self.name == "Majeur" :
+            for note in dico_note.keys() :
+                Lettre_note = ["C", "D", "E", "F", "G", "A", "B"]
+                octave = int((dico_note[note]*5)/60 + 0.99)
+                lettre = dico_note[note] % 7
+                dico_note[note] = Lettre_note[lettre - 1] + str(octave)
+                
+        elif self.name == "Mineur" :
+            for note in dico_note.keys() :
+                Lettre_note = ["C", "D", "D#", "F", "G", "G#", "A#"]
+                octave = int((dico_note[note]*5)/60 + 0.99)
+                lettre = dico_note[note] % 7
+                dico_note[note] = Lettre_note[lettre - 1] + str(octave)
+                
+        return dico_note
+            
+        
+
