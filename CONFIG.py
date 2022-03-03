cbd7a4e8a39bc3fa89c6de70e3f7e0bd6708e329
Arkadixit <83871882+Arkadixit@users.noreply.github.com>
2022-03-03 14:33:52 +0100

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -6,6 +6,7 @@ for i in range(1,max+1):
     SEQ["pas"+str(i)] = PARAM
 
 # SEQ['pas1']['note'] = 1 pour modifier (exemple)
+# actuel = [num du pas, numéro du paramètre de l'encodeur PARAM (1:long, 2:bpm, 3:gamme)]
 
 GEN = {"long":0, "bpm":0, "gamme":0, "actuel" : [1,1]}
 
