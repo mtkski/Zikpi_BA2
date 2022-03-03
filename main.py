585fa2de7f6d045a512f3b3cb5385aeef28dc433
PereBlaise <99497144+PereBlaise@users.noreply.github.com>
2022-03-03 18:24:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,15 +42,15 @@ while True:
     # Prise de valeur
     for encoder in encoder_list:
         if encoder.motion_sensor() == "button pressed":
-            GEN, SEQ = encoder.dictionary_modification(None, GEN, SEQ)
+            GEN, SEQ = encoder.action_spe(None, GEN, SEQ)
 
         elif encoder.motion_sensor() == "rotated clockwise":
-            GEN, SEQ = encoder.dictionary_modification("+", GEN, SEQ)
+            GEN, SEQ = encoder.action_spe("+", GEN, SEQ)
 
         elif encoder.motion_sensor() == "rotated counter-clockwise":
-            GEN, SEQ = encoder.dictionary_modification("-", GEN, SEQ)
+            GEN, SEQ = encoder.action_spe("-", GEN, SEQ)
 
     for button in button_list:
         if button.motion_sensor() == "button pressed":
-            GEN = button.dictionary_modification(GEN)
+            GEN = button.action_spe(GEN)
 
