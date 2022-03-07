8e62eb781e203c58533955c267feef9fc5707aa9
mtkski <95864752+mtkski@users.noreply.github.com>
2022-03-07 13:09:11 +0100

    Create Fonctions_envoi_valeur.py

---
+++ Fonctions_envoi_valeur.py
@@ -0,0 +1,18 @@
+from Adafruit_GPIO import I2C
+tca = I2C.get_i2c_device(address=0x70)
+import Adafruit_MCP4725
+import time
+
+def tca_set(mask):
+    """Select one or more channels.
+           chan =   76543210
+           mask = 0b00000000
+    """
+    if mask > 0xff:
+        return
+    tca.writeRaw8(mask)
+    
+    
+def bpm_to_s(bpm):
+    s = (60 / bpm) 
+    return s
