d35c6fd237d434f41cbfb1bbfb7a4a32170a6633
PereBlaise <99497144+PereBlaise@users.noreply.github.com>
2022-03-12 12:01:25 +0100

    Mise à jour 10/03

--- envoi_valeurs.py
+++ envoi_valeurs.py
@@ -2,93 +2,107 @@ from Adafruit_GPIO import I2C
 tca = I2C.get_i2c_device(address=0x70)
 import Adafruit_MCP4725
 import time
-from Fonctions_envoi_valeur import tca_set, bpm_to_s
-
+from Class_Note import Note
+from CONFIG import *
 dac = Adafruit_MCP4725.MCP4725(address=0x60)
 
-SEQ = {}
-for i in range(1,9):
-    PARAM = {"note":2048, "gate":1, "cv1":2048, "cv2":0, "cv3":0, "cv4":0}
-    SEQ["pas"+str(i)] = PARAM
+def tca_set(mask):
+    """Select one or more channels.
+           chan =   76543210
+           mask = 0b00000000
+    """
+    if mask > 0xff:
+        return
+    tca.writeRaw8(mask)
+    
+    
+def bpm_to_s(bpm):
+    s = (60 / bpm) 
+    return s
 
-SEQ['pas1']['gate'] = 0
+def pourcent_to_DAC(pourcent) :
+    valeur_dac = (4096/100) * pourcent
+    return int(valeur_dac)
 
-SEQ['pas1']['cv1'] = 200
-SEQ['pas2']['cv1'] = 500
-SEQ['pas3']['cv1'] = 900
-SEQ['pas4']['cv1'] = 1000
-SEQ['pas5']['cv1'] = 2000
-SEQ['pas6']['cv1'] = 2800
-SEQ['pas7']['cv1'] = 3400
-SEQ['pas8']['cv1'] = 4096
+def traduction_note_to_DACfct(symbole):
+        dico_note = {}
+        
+        for i in range(1,61,1):
+            Lettre_note = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+            octave = int((i) / 12 + 0.99)
+            lettre = i % 12
+            valeur_dac = i * (4096/59)
+            dico_note[Lettre_note[lettre - 1] + str(octave)] = valeur_dac - (4096/59)
 
-SEQ['pas1']['note'] = int(4096/5)
-SEQ['pas2']['note'] = 0
-SEQ['pas3']['note'] = int(4096/4)
-SEQ['pas4']['note'] = 0
-SEQ['pas5']['note'] = int(4096/3)
-SEQ['pas6']['note'] = 0
-SEQ['pas7']['note'] = int(4096/2)
-SEQ['pas8']['note'] = 0
+        return int(dico_note.get(symbole))
+
 
 #print(SEQ)
-duree_pas = bpm_to_s(int(60))
+duree_pas = bpm_to_s(int(GEN['bpm']))
 #print(duree_pas)
-""""when playpas is pressed
-tca_set(0b0000100)                       # cv1
-    dac.set_voltage(SEQ['pas'+ i ]['cv1'])
 
-    tca_set(0b0001000)                       # cv2
-    dac.set_voltage(SEQ['pas' + i]['cv2'])
 
-    tca_set(0b0010000)                       # cv3
-    dac.set_voltage(SEQ['pas' + i]['cv3'])
 
-    tca_set(0b0100000)                       # cv4
-    dac.set_voltage(SEQ['pas' + i]['cv4'])
 
-    tca_set(0b0100000)                       # note
-    dac.set_voltage(SEQ['pas' + i]['note'])
+class Melody :
+    
+    def __init__(self):
+        self.play = False
 
-    tca_set(0b0100000)                       # gate
-    dac.set_voltage(4096)
-    time.sleep(SEQ['pas' + i]['gate'] * duree_pas)
-    dac.set_voltage(0)
-"""
+    def joueSEQ(self):
+        global duree_pas
+    
+        if self.play == True :
+            self.play = False
+        elif self.play == False :
+            self.play = True
+    
+        while self.play == True :
+            for i in range(1,GEN['long']+1) : #play
+                if self.play == True:
+                    tca_set(0b0000100)                       # cv1
+                    dac.set_voltage(pourcent_to_DAC(SEQ['pas'+ str(i) ]['cv1']))
+                
+                if self.play == True:
+                    tca_set(0b0001000)                       # cv2
+                    dac.set_voltage(pourcent_to_DAC(SEQ['pas' + str(i)]['cv2']))
+                
+                if self.play == True:
+                    tca_set(0b0010000)                       # cv3
+                    dac.set_voltage(pourcent_to_DAC(SEQ['pas' + str(i)]['cv3']))
+                
+                if self.play == True:
+                    tca_set(0b0100000)                       # cv4
+                    dac.set_voltage(pourcent_to_DAC(SEQ['pas' + str(i)]['cv4']))
+                
+                if self.play == True:
+                    tca_set(0b0000001)                       # note
+                    dac.set_voltage(traduction_note_to_DACfct(SEQ['pas' + str(i)]['note']))
+                
+                if self.play == True:
+                    tca_set(0b0000010)                       # gate
+                    dac.set_voltage(4096)
+                    time.sleep(SEQ['pas' + str(i)]['gate'] * duree_pas)
+                    dac.set_voltage(0)
+                    time.sleep(duree_pas - (SEQ['pas' + str(i)]['gate'] * duree_pas))
+    def jouePAS(self) :
+        pas = GEN['actuel'][0]
+        tca_set(0b0000100)                       # cv1
+        dac.set_voltage(pourcent_to_DAC(SEQ['pas'+ str(pas) ]['cv1']))
 
-#for play press
-while True :
-    for i in range(1,9) : #play
-        tca_set(0b00001000)                       # cv1
-        dac.set_voltage(SEQ['pas'+ str(i) ]['cv1'])
-        #print(SEQ['pas'+ str(i) ]['cv1']   )
-        #print(SEQ['pas'+ str(i) ]['cv1'])
-        """
+        tca_set(0b0001000)                       # cv2
+        dac.set_voltage(pourcent_to_DAC(SEQ['pas' + str(pas)]['cv2']))
 
-        tca_set(0b00000000)                       # cv2
-        dac.set_voltage(SEQ['pas' + str(i)]['cv2'])
-        
-        tca_set(0b00000000)                       # cv3
-        dac.set_voltage(SEQ['pas' + str(i)]['cv3'])
+        tca_set(0b0010000)                       # cv3
+        dac.set_voltage(pourcent_to_DAC(SEQ['pas' + str(pas)]['cv3']))
 
-        tca_set(0b00000000)                       # cv4
-        dac.set_voltage(SEQ['pas' + str(i)]['cv4'])
-        """
-        
-        
-        tca_set(0b00000010)                       # gate
+        tca_set(0b0100000)                       # cv4
+        dac.set_voltage(pourcent_to_DAC(SEQ['pas' + str(pas)]['cv4']))
+
+        tca_set(0b0000001)                       # note
+        dac.set_voltage(traduction_note_to_DACfct(SEQ['pas' + str(pas)]['note']))
+
+        tca_set(0b0000010)                       # gate
         dac.set_voltage(4096)
-        #print('volt set')
-        time.sleep(SEQ['pas' + str(i)]['gate'] * duree_pas)
+        time.sleep(SEQ['pas' + str(pas)]['gate'] * duree_pas)
         dac.set_voltage(0)
-        #print('volt 0')
-        time.sleep(duree_pas - SEQ['pas' + str(i)]['gate'] * duree_pas )
-        
-        tca_set(0b00000001)                       # note
-        dac.set_voltage(SEQ['pas' + str(i)]['note'])
-        print(SEQ['pas'+ str(i) ]['note'])
-        
-tca_set(0b00000010)
-dac.set_voltage(0)
-#test bnidon
-#dfgeg
