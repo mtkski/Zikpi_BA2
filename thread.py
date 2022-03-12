c056006dcf5ab3f08640da8418ff5124ab7ea1c4
PereBlaise <99497144+PereBlaise@users.noreply.github.com>
2022-03-12 11:57:57 +0100

    Mise à jour 10/03

--- thread.py
+++ thread.py
@@ -2,28 +2,30 @@
 import RPi.GPIO as GPIO
 import threading
 from CONFIG import Encodeur, Bouton, GEN, SEQ
-from Class_Button import Button
 from Class_Encoder import Encoder
 from Class_Note import Note
+from gpiozero import Button
+from envoi_valeurs import *
+from Affichageécran import *
+from envoi_valeurs import *
 
 global GEN, SEQ
 
 # Pour déterminer quel système de numérotation des pins on utilise
 GPIO.setmode(GPIO.BCM)
 
-# Crée une liste d'objet pour les gammess
+# Crée une liste d'objet pour les gammes
 chromatique = Note(1)
 majeur = Note(2)
 mineur = Note(3)
 liste_gam = [chromatique,majeur,mineur]
 
-# Crée une liste d'objet button et configure chaque canal comme entrée ou sortie pour les boutons
-button_dico = {}
-for button_name in Bouton.keys():
-    PIN = Bouton[button_name]
-    GPIO.setup(PIN, GPIO.IN)
-    button = Button(button_name, PIN)
-    button_dico[button_name] = button
+melodie = Melody()
+
+bouton_play = Button(18)
+bouton_playpas = Button(23)
+bouton_next = Button(14)
+bouton_prev = Button(15)
 
 # Crée une liste d'objet encoder et configure chaque canal comme entrée ou sortie pour les encodeurs
 encoder_dico = {}
@@ -37,20 +39,56 @@ for encoder_name in Encodeur.keys():
         GPIO.setup(SW, GPIO.IN)
     encoder = Encoder(encoder_name, liste_gam, CLK, DT, SW)
     encoder_dico[encoder_name] = encoder
+    
+def pressed_play():
+    thread = threading.Thread(target=melodie.joueSEQ)
+    thread.start()
+    
+def pressed_playpas():
+    melodie.jouePAS()
 
+def pressed_next():
+    GEN["actuel"][0] =  (GEN["actuel"][0] + 1) % (GEN["long"] +1)
+    if GEN["actuel"][0] == 0:
+        GEN["actuel"][0] = 1
+    print(GEN["actuel"])
+    affichervaleurs('pas' + str(GEN['actuel'][0]))
+        
+def pressed_prev():
+    GEN["actuel"][0] -= 1
+    if GEN["actuel"][0] == 0:
+        GEN["actuel"][0] = GEN['long']
+    print(GEN["actuel"])
+    affichervaleurs('pas' + str(GEN['actuel'][0]))
+    
+print(GEN["actuel"])
 # Boucle
 while True:
 
     thread_list = []
-    for encodeur_nom in encoder_dico.keys():
-        thread = threading.Thread(target=encoder_dico[encodeur_nom].motion_sensor)
+    for encoder in encoder_dico.values():
+        thread = threading.Thread(target=encoder.motion_sensor)
         thread.start()
         thread_list.append(thread)
 
-    for bouton_nom in button_dico.keys():
-        thread = threading.Thread(target=button_dico[bouton_nom].motion_sensor)
-        thread.start()
-        thread_list.append(thread)
+    bouton_play.when_pressed = pressed_play
+    bouton_playpas.when_pressed = pressed_playpas
+    bouton_next.when_pressed = pressed_next
+    bouton_prev.when_pressed = pressed_prev
 
-    for thread in thread_list :
-        thread.join()
+
+    for encoder in encoder_dico.values():
+        if encoder.etat == 'button encoder pressed':
+            GEN, SEQ = encoder.action_spe(None, GEN, SEQ)
+            print(SEQ['pas1'])
+            affichervaleurs('pas' + str(GEN['actuel'][0]))
+
+        elif encoder.etat == "rotated clockwise":
+            GEN, SEQ = encoder.action_spe("+", GEN, SEQ)
+            print(SEQ['pas1'])
+            affichervaleurs('pas' + str(GEN['actuel'][0]))
+
+        elif encoder.etat == "rotated counter-clockwise":
+            GEN, SEQ = encoder.action_spe("-", GEN, SEQ)
+            print(SEQ['pas1'])
+            affichervaleurs('pas' + str(GEN['actuel'][0]))
