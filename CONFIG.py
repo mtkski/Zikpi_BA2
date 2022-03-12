c608b181c5a279a21b0580948a5ef456954b9027
PereBlaise <99497144+PereBlaise@users.noreply.github.com>
2022-03-12 12:00:15 +0100

    Mise à jour 10/03

--- CONFIG.py
+++ CONFIG.py
@@ -2,36 +2,24 @@
 max = 64
 SEQ = {}
 for i in range(1,max+1):
-    PARAM = {"note":'C1', "gate":0, "cv1":0, "cv2":0, "cv3":0, "cv4":0}
+    PARAM = {"note":'C2', "gate":0.5, "cv1":0, "cv2":0, "cv3":0, "cv4":0}
     SEQ["pas"+str(i)] = PARAM
 
 # SEQ['pas1']['note'] = 1 pour modifier (exemple)
 # actuel = [num du pas, numéro du paramètre de l'encodeur PARAM (1:long, 2:bpm, 3:gamme)]
 
-GEN = {"long":64, "bpm":250, "gamme":1, "actuel" : [1,1]}
-
-
-# pins et autres (adapter avec le produit de philippe)
+GEN = {"long":8, "bpm":120, "gamme":1, "actuel" : [1,1]}
 
 # Définit les pins utilisés par les encodeurs
 Encodeur = {
-    "encodeur_NOTE" : [5,6,10],
-    "encodeur_GATE" : [9,11,None],
+    "encodeur_NOTE" : [10,9,11],
+    "encodeur_GATE" : [5,6,None],
     "encodeur_CV1" : [20,21,None],
     "encodeur_CV2" : [12,16,None],
     "encodeur_CV3" : [8,7,None],
     "encodeur_CV4" : [24,25,None],
     "encodeur_PARAM" : [13,19,26]
     }
-
-# Définit les valeurs initiales des encodeurs
-val = [0,0,0,0,0,0,0]
-
-# Définit les paliers en fonction des encodeurs
-step = [int(4096/(12*5)),1,1,1,1,1,1]
-
-# Définit l'intervalle des encodeurs
-interval = [(0,4096),(0,1),(0,4096),(0,4096),(0,4096),(0,4096),(0,4096)]
 
 # Définit les pins utilisés par les Boutons
 Bouton = {
@@ -40,4 +28,3 @@ Bouton = {
     "bouton_PREV" : 15,
     "bouton_NEXT" : 14,
         }
-
