cf0f6897eca12d000373f6051f8c9a04291b16de
PereBlaise <nathan.francois@ulb.be>
2022-03-14 13:50:02 +0100

    correction

--- Affichageécran.py
+++ Affichageécran.py
@@ -1,15 +1,34 @@
 import time
 import LCD
 from CONFIG import SEQ
+class Affichage:
 
-def affichervaleurs(pas):
-    ICI=SEQ[pas]
-    mylcd =LCD.lcd()
-    mylcd.lcd_display_string(ICI['note'],1)
-    mylcd.lcd_display_string(str(ICI['gate']),2)
-    mylcd.lcd_display_string(str(ICI['cv1']),1,4)
-    mylcd.lcd_display_string(str(ICI['cv2']),2,4)
-    mylcd.lcd_display_string(str(ICI['cv3']),1,8)
-    mylcd.lcd_display_string(str(ICI['cv4']),2,8)
-    mylcd.lcd_display_string('PARA',1,12)
-    mylcd.lcd_display_string('VALE',2,12)
+    def note(self, pas):
+        ICI=SEQ[pas]
+        mylcd =LCD.lcd()
+        mylcd.lcd_display_string(ICI['note'], 1)
+    def gate(self,pas):
+        ICI = SEQ[pas]
+        mylcd = LCD.lcd()
+        mylcd.lcd_display_string(str(ICI['gate']),2)
+    def cv1(self, pas):
+        ICI = SEQ[pas]
+        mylcd = LCD.lcd()
+        mylcd.lcd_display_string(str(ICI['cv1']),1,4)
+    def cv2(self,pas):
+        ICI = SEQ[pas]
+        mylcd = LCD.lcd()
+        mylcd.lcd_display_string(str(ICI['cv2']),2,4)
+    def cv3(self,pas):
+        ICI = SEQ[pas]
+        mylcd = LCD.lcd()
+        mylcd.lcd_display_string(str(ICI['cv3']),1,8)
+    def cv4(self,pas):
+        ICI = SEQ[pas]
+        mylcd = LCD.lcd()
+        mylcd.lcd_display_string(str(ICI['cv4']),2,8)
+    def param(self,pas):
+        ICI = SEQ[pas]
+        mylcd = LCD.lcd()
+        mylcd.lcd_display_string('PARA',1,12)
+        mylcd.lcd_display_string('VALE',2,12)
