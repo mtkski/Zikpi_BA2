20be10e368b77a9c359fcf46b4c70c1f47cb5f11
PereBlaise <99497144+PereBlaise@users.noreply.github.com>
2022-03-16 14:36:10 +0100

    Update CONFIG_2.py

--- CONFIG_2.py
+++ CONFIG_2.py
@@ -1,3 +1,6 @@
+from gpiozero import Button
+from gpiozero import RotaryEncoder
+
 max = 64
 SEQ = {}
 for i in range(1,max+1):
@@ -5,9 +8,10 @@ for i in range(1,max+1):
     SEQ["pas"+str(i)] = PARAM
 
 # SEQ['pas1']['note'] = 1 pour modifier (exemple)
-# actuel = [num du pas, numéro du paramètre de l'encodeur PARAM (1:long, 2:bpm, 3:gamme)]
+# actuel = [num du pas, numéro du paramètre de l'encodeur PARAM (0:long, 1:bpm, 2:gamme)]
 
-GEN = {"long":8, "bpm":120, "gamme":1, "actuel" : [1,1]}
+GEN = {"long":8, "bpm":120, "gamme":1, "actuel" : [1,0]}
+
 
 bouton_play = Button(18)
 bouton_playpas = Button(23)
